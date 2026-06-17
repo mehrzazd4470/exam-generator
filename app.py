3753482932f0e2d01d17959ad558e62ccabcb1af
mehrzazd4470 <mehrzad.82.11.8@gmail.com>
2026-06-17 21:57:13 +0330

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ AVALAI_BASE_URL = "https://api.avalai.ir/v1"
 # ============================
 
 # ====== تنظیمات OpenRouter ======
-OPENROUTER_API_KEY = "sk-or-v1-..."  # کلید خود را از openrouter.ai بگیرید
+OPENROUTER_API_KEY = "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"  # کلید خود را از openrouter.ai بگیرید
 OPENROUTER_BASE_URL = "https://openrouter.ai/api/v1"
 # ============================
 
